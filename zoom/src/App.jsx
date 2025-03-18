3973bcd59b4055a2a314b3d36708667feab73c5d
serhat-bca <sersen@bergen.org>
2025-03-18 15:59:26 -0400

    push for nothing

--- zoom/src/App.jsx
+++ zoom/src/App.jsx
@@ -1,11 +1,15 @@
+import { useSelector, useDispatch } from "react-redux";
+import { zoomIn, zoomOut, reset } from "./actions/zoomActions";
+
 const App = () => {
-  const zoomLevel = 100;
+  const zoomLevel = useSelector((state) => state.zoomLevel);
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>Zoom Setup</h2>
-      <button>Zoom In</button>
-      <button>Reset</button>
-      <button>Zoom out</button>
+      <button onClick={() => dispatch(zoomIn())}>Zoom In</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+      <button onClick={() => dispatch(zoomOut())}>Zoom out</button>
       <p>Zoom Level: {zoomLevel}</p>
     </div>
   );
---
+++ zoom/src/actions/zoomActions.js
@@ -0,0 +1,15 @@
+export const zoomIn = () => {
+  return {
+    type: "ZOOM_IN",
+  };
+};
+
+export const zoomOut = () => ({
+  type: "ZOOM_OUT",
+});
+
+export const reset = () => {
+  return {
+    type: "RESET",
+  };
+};
