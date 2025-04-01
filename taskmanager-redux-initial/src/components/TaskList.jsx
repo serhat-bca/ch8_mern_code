96c84d9afc9fb9983e3ece92e4a49cda80fa275b
serhat-bca <sersen@bergen.org>
2025-04-01 16:14:25 -0400

    final commit on Tuesday

--- taskmanager-redux-initial/src/components/TaskList.jsx
+++ taskmanager-redux-initial/src/components/TaskList.jsx
@@ -4,12 +4,20 @@ import { useSelector } from "react-redux";
 
 const TaskList = () => {
   const tasks = useSelector((state) => state.tasks);
+  const filter = useSelector((state) => state.filter);
+
+  const filteredTasks =
+    filter === "all"
+      ? tasks
+      : filter === "urgent"
+      ? tasks.filter((task) => task.urgent)
+      : tasks.filter((task) => !task.urgent);
 
   return (
     <div>
       <h2>Task List</h2>
       <FilterTask />
-      {tasks.map((task) => (
+      {filteredTasks.map((task) => (
         <Task key={task.id} task={task} />
       ))}
     </div>
